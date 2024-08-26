64a7c4822891ede2028041efeb5a130005626ded
Dimitris-Toulis <dimitritouli@gmail.com>
2024-08-26 20:54:17 +0300

    Update game.js

--- game.js
+++ game.js
@@ -5,28 +5,45 @@
 @addedOn: 2024-07-31
 */
 /*
+**There are 3 possible endings: Normal, Greed and Happy**
+
+There is also lore and hidden messages! Try to find them!
+
 ## Controls
-- Move with wasd  
-- Restart with double k  
-- Cycle through orbs with j and l  
-- Use selected orb with i (if it has an action)
+- Move with WASD
+- Restart with double K 
+- Cycle through orbs with J and L  
+- Use selected orb with I (if it has an active effect)
 
 ## Orbs
+
+Passive effects are enabled when holding the orb and active effects are used by pressing I
+
+List of orbs in order of collection:
 - Destruction orb: Use it to destroy all destructible objects around you  
-- Ghost orb: Pass through normal walls, crates and rocks  
+- Ghost orb: Pass through normal (grey) walls, crates and rocks  
 - Fire orb: Turns water into smoke that disappears afterwards  
-- Water orb: Turns lava into obsidian to pass over it. Use it to create water
-- Invisibility orb: Enemies can't see you and therefore don't kill you
-- Electric orb: Use it to activate machines touching you. It will also kill you instantly in water if held!
-- Transform orb: Turns you into a plant that does not trigger human traps
-- Ultimate orb: Makes you immortal. Use it to win the game
+- Water orb: Turns lava into obsidian, allowing you to pass over it. Use it to create water
+- Invisibility orb: Enemies can't see you
+- Electric orb: Use it to activate machines adjacent you. It will also kill you instantly in water if held!
+- Transform orb: Turns you into a plant that does not trigger traps
+- Ultimate orb: Makes you immortal. Use it to win the game?
 
 ## Special tiles
-- Regenerating lava: Turns back to lava after 6 "moves" (i,j,k,l also count)
+- Lava: Kills you if you step on it
+- Regenerating lava: Turns back to lava after 6 "moves" (all buttons count) if turned into obsidian
+- Water: Breathing mechanic (4 points of breath). Breath is deducted for moving in water. Getting out restores your breath
+- Enemy: Kills you if they see you on a neighboring tile (diagonals also)
+- Trap: Kills players (So if you are not a player you don't die)
+- Smoke: Disappears after 2 moves. Produced by water+fire orb and gate machines
 - Machines: Activate them using the electric orb
   - Water machine: Kills you by mixing water and electricity
   - Plant machine: Spawns a plant on top of you
   - Gate machine: Destroys gates and turns into smoke
+- Arrow (black): Suggests a direction to go to
+- Arrow (red): Enforces a direction to go to
+- Altar: ???
+
 */
 
 const player = "p"
